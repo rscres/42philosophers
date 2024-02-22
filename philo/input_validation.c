9d50e197ecbc3bc63f0edc7a1deeba78342d8fe3
rscres <rseelaen@student.42sp.org.br>
2024-02-22 14:11:12 -0300

    input_validation: separate is num and is larger than 0 in different checks

--- philo/input_validation.c
+++ philo/input_validation.c
@@ -3,14 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   input_validation.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: renato <renato@student.42.fr>              +#+  +:+       +#+        */
+/*   By: rseelaen <rseelaen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/14 01:51:55 by renato            #+#    #+#             */
-/*   Updated: 2024/02/21 20:25:40 by renato           ###   ########.fr       */
+/*   Updated: 2024/02/22 14:09:39 by rseelaen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+int	is_positive(char **str)
+{
+	int		i;
+
+	i = 1;
+	while (str[i])
+	{	
+		if (ft_atoi(str[i]) <= 0)
+		{
+			printf("Error: arguments must be larger than 0\n");
+			return (0);
+		}
+		i++;
+	}
+	return (1);
+}
 
 int	is_num(char **str)
 {
@@ -21,14 +38,9 @@ int	is_num(char **str)
 	while (str[i])
 	{	
 		tmp = str[i];
-		if (tmp[0] == '-' || (i != 5 && tmp[0] == '0'))
-		{
-			printf("Error: arguments must be larger than 0\n");
-			return (0);
-		}
 		while (*tmp)
 		{
-			if (*tmp < '0' || *tmp > '9' )
+			if ((*tmp < '0' || *tmp > '9') && *tmp != '-')
 			{
 				printf("Error: argument is not a number\n");
 				return (0);
@@ -60,6 +72,8 @@ int	check_input(int argc, char **argv)
 	if (num_of_args(argc))
 		return (1);
 	if (!is_num(argv))
+		return (1);
+	if (!is_positive(argv))
 		return (1);
 	return (0);
 }
